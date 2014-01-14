4f41a4fac628ff65808350c0f480461f30e56cba
way <waynewgl@gmail.com>
2014-01-14 21:42:25 +0800

    add apipie

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ FunnyRLife::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
 
-  resources :user
+  #resources :user
 
 
   root :to => 'user#index'
