6550643753fd0ba38c2f60d5da4ccd0f59ee45a8
way <waynewgl@gmail.com>
2014-01-17 15:52:49 +0800

    add push  func

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,9 +10,8 @@ class UserController < ApplicationController
 
       logger.info "now we have the post #{post.content}"
     end
+
   end
-
-
 
 
   api :POST, "/user/dev_pushTest", "测试通知推送服务"
@@ -65,7 +64,7 @@ class UserController < ApplicationController
   api :POST, "/user/register", "注册新用户"
   param :account, String, "用户账户号", :required => true
   param :password, String, "用户密码", :required => true
-  param :token, String, "用户的设备TOKEN", :required => true
+  param :token, String, "用户的设备TOKEN, 手机端必须提供此参数实现通知推送服务", :required => false
 
   description <<-EOS
     用于注册新用户
@@ -99,12 +98,12 @@ class UserController < ApplicationController
       msg[:code] = 233
       msg[:description] = "缺少参数: account 或者 password 或者 token"
       render :json =>  msg.to_json
-
+      return
     else
 
       new_user = User.new
       new_user.account = params[:account]
-      new_user.password = params[:password]
+      new_user.password =  Digest::SHA1.hexdigest(params['password'])
       new_user.token = params[:token]
 
       if new_user.save
@@ -128,12 +127,62 @@ class UserController < ApplicationController
 
 
 
+  api :POST, "/user/user_login", "用户登录"
+  param :account, String, "用户账户号", :required => true
+  param :password, String, "用户密码", :required => true
+  param :token, String, "用户的设备TOKEN, 手机端必须提供此参数实现通知推送服务", :required => false
+
+  description <<-EOS
+    用于注册新用户
+    注册成功 返回：
+    {
+      code: 200
+      user_id:1
+      description:“用户登录成功”
+    }
+
+    注册失败 返回
+    {
+      code:333
+      description:“该用户不存在”
+    }
+
+    参数缺少 返回
+    {
+      code:223
+      description:“缺少必须参数”
+    }
+
+  EOS
   def user_login
 
+    msg_userLogin = Hash.new
 
-    render :inline => "login succeed"
-    logger.info "received  use account #{params[:username]}  and  password #{params[:password]}"
+    if params[:account].nil? || params[:password].nil?
+
+      msg_userLogin[:code] = 233
+      msg_userLogin[:description] = "缺少参数: account 或者 password"
+      render :json =>  msg_userLogin.to_json
+      return
+    end
+
+    user = User.where("account = ? and password = ?", params[:account], Digest::SHA1.hexdigest(params[:password]) ).first
+
+    if user.nil?
+
+      msg_userLogin[:code] = 333
+      msg_userLogin[:user_id] = nil
+      msg_userLogin[:description] = "该用户不存在"
+      render :json =>  msg_userLogin.to_json
+
+    else
+      msg_userLogin[:code] = 200
+      msg_userLogin[:user_id] = user.id
+      msg_userLogin[:description] = "登陆成功"
+      render :json =>  msg_userLogin.to_json
+
+    end
+
   end
-
 
 end
