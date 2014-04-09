8baa240dcb7889701c4950f9e2f5e197cb42e246
way <waynewgl@gmail.com>
2014-04-09 10:18:05 +0800

    delete

--- db/migrate/20140109034546_create_users.rb
+++ db/migrate/20140109034546_create_users.rb
@@ -2,11 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
 
-<<<<<<< HEAD
       t.string :firstName
-=======
-      t.string :frstName
->>>>>>> 3bc6584b50023a0852eff38f1380a687bfc0eeb9
       t.string :lastName
       t.string :account
       t.string :password
