deb00c572c3d5e790bae68a32ed1cc82a52442df
way <waynewgl@gmail.com>
2014-04-09 12:21:57 +0800

    delete

--- db/migrate/20140223034704_add_column_to_post.rb
+++ db/migrate/20140223034704_add_column_to_post.rb
@@ -2,6 +2,8 @@ class AddColumnToPost < ActiveRecord::Migration
   def change
     add_column :posts, :fever_id, :string
     add_column :posts, :meature_time, :string
-
+    add_column :posts, :title, :string
+    add_column :posts, :date, :string
+    add_column :posts, :author, :string
   end
 end
